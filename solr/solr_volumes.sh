f8b5b6421227c4ab169023f2791312a4fd141d69
jweigend <johannes.weigend@qaware.de>
2018-12-05 11:09:25 +0100

    solr

--- solr/solr_volumes.sh
+++ solr/solr_volumes.sh
@@ -19,5 +19,5 @@ pssh -H "${JOIN_NODES[*]}" -t 600 -i mkdir -p $PV_PATH/{0,1,2,3,4,5,6,7,8,9}
 
 helm install ${0%/*}/../pv --name solr-volumes \
 --set nodes="{$JOIN_NODES_COMMA_SEP}" --set size=$PV_SIZE \
---set volumes=$PV_VOLUMES_PER_NODE --set storageClassName=$PV_STORAGE_CLASS_NAME\
+--set volumes=$PV_VOLUMES_PER_NODE --set storageClassName=$PV_STORAGE_CLASS_NAME \
 --set path=$PV_PATH
