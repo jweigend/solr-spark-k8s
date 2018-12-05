7c7b2a86c4e426109339070cb55fb14c4908b5e6
Johannes Weigend <johannes.weigend@qaware.de>
2018-12-05 17:46:25 +0100

    ok

--- baremetal/setup-node.sh
+++ baremetal/setup-node.sh
@@ -26,25 +26,25 @@ function downloadSoftware() {
     mv zookeeper-3.4.13 ../software
     ln -s /opt/software/zookeeper-3.4.13 /opt/zookeeper
 
-    # curl -O http://apache.lauf-forum.at/lucene/solr/7.5.0/solr-7.5.0.tgz
-    # tar xvf solr-7.5.0.tgz 
-    # mv solr-7.5.0 ../software
-    # ln -s /opt/software/solr-7.5.0 /opt/solr
+    curl -O http://apache.lauf-forum.at/lucene/solr/7.5.0/solr-7.5.0.tgz
+    tar xvf solr-7.5.0.tgz 
+    mv solr-7.5.0 ../software
+    ln -s /opt/software/solr-7.5.0 /opt/solr
 
-    # curl -O http://apache.lauf-forum.at/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
-    # tar xvf spark-2.3.2-bin-hadoop2.7.tgz 
-    # mv spark-2.3.2-bin-hadoop2.7 ../software
-    # ln -s /opt/software/spark-2.3.2-bin-hadoop2.7 /opt/spark
+    curl -O http://apache.lauf-forum.at/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz
+    tar xvf spark-2.3.2-bin-hadoop2.7.tgz 
+    mv spark-2.3.2-bin-hadoop2.7 ../software
+    ln -s /opt/software/spark-2.3.2-bin-hadoop2.7 /opt/spark
 
-    # curl -O http://apache.lauf-forum.at/zeppelin/zeppelin-0.8.0/zeppelin-0.8.0-bin-all.tgz
-    # tar xvf zeppelin-0.8.0-bin-all.tgz 
-    # mv zeppelin-0.8.0-bin-all ../software
-    # ln -s /opt/software/zeppelin-0.8.0-bin-all /opt/zeppelin
+    curl -O http://apache.lauf-forum.at/zeppelin/zeppelin-0.8.0/zeppelin-0.8.0-bin-all.tgz
+    tar xvf zeppelin-0.8.0-bin-all.tgz 
+    mv zeppelin-0.8.0-bin-all ../software
+    ln -s /opt/software/zeppelin-0.8.0-bin-all /opt/zeppelin
 
-    # curl -O http://apache.lauf-forum.at/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
-    # tar xvf hadoop-2.7.7.tar.gz
-    # mv hadoop-2.7.7 ../software
-    # ln -s /opt/software/hadoop-2.7.7 /opt/hadoop
+    curl -O http://apache.lauf-forum.at/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
+    tar xvf hadoop-2.7.7.tar.gz
+    mv hadoop-2.7.7 ../software
+    ln -s /opt/software/hadoop-2.7.7 /opt/hadoop
 
     popd
 }
@@ -63,9 +63,9 @@ function configureSoftware() {
 function configureZookeeper() {
     mkdir -p /var/lib/zookeeper
     mkdir -p /opt/zookeeper/conf
-    cp ./zookeeper/zoo.cfg /opt/zookeeper/conf
+    #cp ./zookeeper/zoo.cfg /opt/zookeeper/conf
     hostname | sed -r 's/node([0-9]*)\..*/\1/g' > /var/lib/zookeeper/myid
-    cp ./zookeeper/start-zk.sh /opt
+    #cp ./zookeeper/start-zk.sh /opt
 }
 
 function configureSolr() {
--- baremetal/setup-nodes.sh
+++ baremetal/setup-nodes.sh
@@ -4,4 +4,5 @@
 
 export CLUSTER_NODES=(node4.cloud node3.cloud node2.cloud node1.cloud node0.cloud)
 
-pssh -H "${CLUSTER_NODES[*]}" -t 600 -o ${0%/*}/../logs -I<${0%/*}/setup-node.sh
+pssh -H "${CLUSTER_NODES[*]}" -t 6000 -o ${0%/*}/../logs -I<./setup-node.sh
+
