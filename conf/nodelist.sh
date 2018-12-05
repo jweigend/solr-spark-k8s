5adc32d5f91be7eed947f78544c0e2bd5fa38381
jweigend <johannes.weigend@qaware.de>
2018-12-05 10:33:55 +0100

    nodelist

---
+++ conf/nodelist.sh
@@ -0,0 +1,5 @@
+export CLUSTER_NODES=(node4.cloud node3.cloud node2.cloud node1.cloud node0.cloud)
+export JOIN_NODES=(node4.cloud node3.cloud node2.cloud node1.cloud)
+export MASTER_NODE=node0.cloud
+export CLUSTER_NODES_COMMA_SEP=node4.cloud,node3.cloud,node2.cloud,node1.cloud,node0.cloud
+export JOIN_NODES_COMMA_SEP=node4.cloud,node3.cloud,node2.cloud,node1.cloud
